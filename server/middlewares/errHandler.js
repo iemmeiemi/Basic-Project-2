2c73802b6a053eb1100eb01d143c7a8042eb11c7
nguyenlinh <rongxanhbidien@gmail.com>
2023-10-01 00:19:17 +0700

    5. USER: Forgot And Reset Password --fix-route-not-found

--- server/middlewares/errHandler.js
+++ server/middlewares/errHandler.js
@@ -1,7 +1,7 @@
 const notFound = (req, res, next) => {
     const error = new Error(`Route ${req.originalUrl} not found!`);
     res.status(404);
-    next();
+    next(error);
 }
 
 const errHandler = (err, req, res, next) => {
